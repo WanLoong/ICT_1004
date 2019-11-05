33e20286c43023684aead80e400330a160f2dc90
lusterless <57383960+lusterless@users.noreply.github.com>
2019-11-05 16:50:50 +0800

    changes
    changes

--- cuisinesKQ.php
+++ cuisinesKQ.php
@@ -18,6 +18,7 @@ and open the template in the editor.
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> 
         <script src="js/sideMenu.js"></script>
+        <script src="js/cuisinesJS.js"></script>
 
     </head>
     <body>
@@ -1060,103 +1061,6 @@ and open the template in the editor.
     </div>
 
 </footer>-->
-
-
-
-        <script>
-            function textBold(x) {
-                if (x == "0") {
-                    document.getElementsByClassName("changecolor")[0].style.color = "black";
-                } else if (x == "1")
-                {
-                    document.getElementsByClassName("changecolor")[1].style.color = "black"
-                }
-            }
-            function textNotBold(x) {
-                if (x == "0") {
-                    document.getElementsByClassName("changecolor")[0].style.color = "#adadad";
-                } else if (x == "1")
-                {
-                    document.getElementsByClassName("changecolor")[1].style.color = "#adadad";
-                }
-            }
-
-
-            document.getElementById("modalButton").onclick = function () {
-                location.href = "shoppingCartNel.php";
-            };
-            document.getElementById("modalButton2").onclick = function () {
-                location.href = "shoppingCartNel.php";
-            };
-            document.getElementById("modalButton3").onclick = function () {
-                location.href = "shoppingCartNel.php";
-            };
-            document.getElementById("modalButton4").onclick = function () {
-                location.href = "shoppingCartNel.php";
-            };
-            document.getElementById("modalButton5").onclick = function () {
-                location.href = "shoppingCartNel.php";
-            };
-            document.getElementById("modalButton6").onclick = function () {
-                location.href = "shoppingCartNel.php";
-            };
-            document.getElementById("modalButton7").onclick = function () {
-                location.href = "shoppingCartNel.php";
-            };
-            document.getElementById("modalButton8").onclick = function () {
-                location.href = "shoppingCartNel.php";
-            };
-            document.getElementById("modalButton9").onclick = function () {
-                location.href = "shoppingCartNel.php";
-            };
-            document.getElementById("modalButton10").onclick = function () {
-                location.href = "shoppingCartNel.php";
-            };
-            document.getElementById("modalButton11").onclick = function () {
-                location.href = "shoppingCartNel.php";
-            };
-            document.getElementById("modalButton12").onclick = function () {
-                location.href = "shoppingCartNel.php";
-            };
-            document.getElementById("modalButton13").onclick = function () {
-                location.href = "shoppingCartNel.php";
-            };
-            document.getElementById("modalButton14").onclick = function () {
-                location.href = "shoppingCartNel.php";
-            };
-            document.getElementById("modalButton15").onclick = function () {
-                location.href = "shoppingCartNel.php";
-            };
-            document.getElementById("modalButton16").onclick = function () {
-                location.href = "shoppingCartNel.php";
-            };
-            document.getElementById("modalButton17").onclick = function () {
-                location.href = "shoppingCartNel.php";
-            };
-            document.getElementById("modalButton18").onclick = function () {
-                location.href = "shoppingCartNel.php";
-            };
-            document.getElementById("modalButton19").onclick = function () {
-                location.href = "shoppingCartNel.php";
-            };
-            document.getElementById("modalButton20").onclick = function () {
-                location.href = "shoppingCartNel.php";
-            };
-            document.getElementById("modalButton21").onclick = function () {
-                location.href = "shoppingCartNel.php";
-            };
-            document.getElementById("modalButton22").onclick = function () {
-                location.href = "shoppingCartNel.php";
-            };
-            document.getElementById("modalButton23").onclick = function () {
-                location.href = "shoppingCartNel.php";
-            };
-            document.getElementById("modalButton24").onclick = function () {
-                location.href = "shoppingCartNel.php";
-            };
-
-
-        </script>
 
 
     </body>
