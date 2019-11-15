5de4c4b6388439171c79368cf645306f8831fb4f
nelsonyeo27 <57286509+nelsonyeo27@users.noreply.github.com>
2019-11-15 23:49:41 +0800

    .

--- shoppingCartNel.php
+++ shoppingCartNel.php
@@ -98,79 +98,72 @@ if(isset($_POST["index"]))
 <?php
 if($i<=0)
 {
-    $productDisplay .= '<h1 style="margin-left:50px;">Your Shopping Cart is Empty.</h1>';
+    $productDisplay .= '<h1>Your Shopping Cart is Empty.</h1>';
     $checkoutbtn = '';
 }
 ?>
-<html>
-    <head>
-        <title>Welcome To Guilty Pleasures!</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="css/bootstrap.css"/>
-        <link rel="stylesheet" href="css/main2.css"/>
-        <link rel="stylesheet" href="css/modalcss.css"/>
-        <!--<link rel="stylesheet" href="css/cusineMenu.css"/>-->
-        <link rel="stylesheet" href="css/shoppingCart_1.css"/>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> 
-        <script src="js/sideMenu.js"></script>
-        <script src="js/Cart.js" async></script>
-
-    </head>
-    <body>
-        
-        <?php 
-            include "headerLogin.php";
-        ?>
-    
-        <!--<div class="container">
-
-        <!--</div>-->
-
-        <h1 style="
-            padding-left: 20px;">Shopping Cart</h1>
-        <div class="shopping-cart">
+<!DOCTYPE html>
+<head>
+    <title>Welcome To Guilty Pleasures!</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.css"/>
+    <link rel="stylesheet" href="css/main2.css"/>
+    <link rel="stylesheet" href="css/modalcss.css"/>
+    <!--<link rel="stylesheet" href="css/cusineMenu.css"/>-->
+    <link rel="stylesheet" href="css/shoppingCart_1.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> 
+    <script src="js/sideMenu.js"></script>
 
 
-            <div class="column-labels">
+</head>
+<body>
 
-                <label class="product-image">Image</label>
-                <label class="product-details">Product</label>
-                <label class="product-price">Price</label>
-                <label class="product-quantity">Quantity</label>
-                <label class="product-removal">Remove</label>
-                <label class="product-line-price">Total</label>
-            </div>
-            <?php echo $productDisplay;
+<?php 
+    include "headerLogin.php";
+?>
 
-            ?>
-            <div class="totals">
-                <div class="totals-item">
-                    <label>Subtotal</label>
-                    <div class="totals-value" id="cart-subtotal"> <?php echo $cartOutput?></div>
-                </div>
-                <div class="totals-item">
-                    <label>Tax (5%)</label>
-                    <div class="totals-value" id="cart-tax"><?php echo $tax?></div>
-                </div>
-                <div class="totals-item">
-                    <label>Delivery</label>
-                    <div class="totals-value" id="cart-shipping"><?php echo $delivery?></div>
-                </div>
-                <div class="totals-item totals-item-total">
-                    <label>Grand Total</label>
-                    <div class="totals-value" id="cart-total"><b><?php echo $carttotal?></b></div>
-                </div>
-                <?php echo $checkoutbtn ?>
 
-                <form  action="cuisinesKQ">
-                    <input class="returntomenu"type="submit" value="Return To Menu"/>
-                </form>
-
-            </div>
+    <h1>Shopping Cart</h1>
+    <div class="shopping-cart">
+        <div class="column-labels">
+            <label class="product-image">Image</label>
+            <label class="product-details">Product</label>
+            <label class="product-price">Price</label>
+            <label class="product-quantity">Quantity</label>
+            <label class="product-removal">Remove</label>
+            <label class="product-line-price">Total</label>
         </div>
-    </body>
+        <?php 
+            echo $productDisplay;
+        ?>
+        <div class="totals">
+            <div class="totals-item">
+                <label>Subtotal</label>
+                <div class="totals-value" id="cart-subtotal"> <?php echo $cartOutput?></div>
+            </div>
+            <div class="totals-item">
+                <label>Tax (5%)</label>
+                <div class="totals-value" id="cart-tax"><?php echo $tax?></div>
+            </div>
+            <div class="totals-item">
+                <label>Delivery</label>
+                <div class="totals-value" id="cart-shipping"><?php echo $delivery?></div>
+            </div>
+            <div class="totals-item totals-item-total">
+                <label>Grand Total</label>
+                <div class="totals-value" id="cart-total"><b><?php echo $carttotal?></b></div>
+            </div>
+            <?php echo $checkoutbtn ?>
+
+            <form  action="cuisinesKQ">
+                <input class="returntomenu"type="submit" value="Return To Menu"/>
+            </form>
+
+        </div>
+    </div>
+</body>
 </html>
